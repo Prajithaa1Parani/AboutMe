22909e721e95d58f57fee358677d461284f4aa8c
Prajithaa <prajithaa.sasikala@gmail.com>
2026-01-02 22:26:59 +0530

    Add resume page and navigation

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def home():
         degree=degree,
         goal=goal
     )
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
